7dc387323886f7296284257636c4542e521b5f96
renzobaldovino <renzo.baldovino@gmail.com>
2023-08-29 15:08:52 +0800

    feat(component): :sparkles: add form message for form errors

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -9,7 +9,6 @@ import { Button } from "@/components/ui/button";
 import {
 	Form,
 	FormControl,
-	FormDescription,
 	FormField,
 	FormItem,
 	FormLabel,
@@ -144,6 +143,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 									onChange={(e) => handleImage(e, field.onChange)}
 								/>
 							</FormControl>
+							<FormMessage />
 						</FormItem>
 					)}
 				/>
@@ -162,6 +162,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 									{...field}
 								/>
 							</FormControl>
+							<FormMessage />
 						</FormItem>
 					)}
 				/>
@@ -180,6 +181,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 									{...field}
 								/>
 							</FormControl>
+							<FormMessage />
 						</FormItem>
 					)}
 				/>
@@ -198,6 +200,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 									{...field}
 								/>
 							</FormControl>
+							<FormMessage />
 						</FormItem>
 					)}
 				/>

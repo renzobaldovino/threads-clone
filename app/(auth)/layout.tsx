446c41aa51948ddef0275cd3e21eddfcadda668a
renzobaldovino <renzo.baldovino@gmail.com>
2023-09-01 14:42:44 +0800

    feat(layout): :lipstick: center clerk sign in and sign up

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
-import'../globals.css';
+import "../globals.css";
 import { Metadata } from "next";
-import {Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
@@ -15,11 +15,15 @@ export default function RootLayout({
 }: {
 	children: React.ReactNode;
 }) {
-	return <ClerkProvider>
-		<html lang="en">
-			<body className={`${inter.className} bg-dark-1`}>
-				{children}
-			</body>
-		</html>
-	</ClerkProvider>;
+	return (
+		<ClerkProvider>
+			<html lang="en">
+				<body className={`${inter.className} bg-dark-1`}>
+					<div className="w-full flex justify-center items-center min-h-screen">
+						{children}
+					</div>
+				</body>
+			</html>
+		</ClerkProvider>
+	);
 }
